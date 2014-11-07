2c386709e9775e00fd80f1e800740633661471e4
Brendan O'Brien <brendnaobrienesq@gmail.com>
2014-11-07 16:54:33 -0500

    playing with standard store object

---
+++ stores/Store.js
@@ -0,0 +1,51 @@
+var EventEmitter = require('events').EventEmitter
+  , underscore = require('underscore')
+
+var CHANGE_EVENT = 'change'
+  , ERROR_EVENT = 'error'
+  , MESSAGE_EVENT = 'message';
+
+var storeProto = {
+  emitChange: function() {
+    this.emit(CHANGE_EVENT);
+  },
+  /**
+   * @param {function} callback
+   */
+  onChange: function(callback) {
+    this.on(CHANGE_EVENT, callback);
+  },
+  /**
+   * @param {function} callback
+   */
+  offChange: function(callback) {
+    this.removeListener(CHANGE_EVENT, callback);
+  },
+
+  emitError : function (err) {
+    this.emit(ERROR_EVENT, err);
+  },
+  onError : function (callback) {
+    this.on(ERROR_EVENT, callback);
+  },
+  offError : function (callback) {
+    this.removeListener(ERROR_EVENT, callback);
+  },
+  
+  emitMessage : function (msg) {
+    this.emit(MESSAGE_EVENT, msg);
+  },
+  onMessage : function (callback) {
+    this.on(MESSAGE_EVENT, callback);
+  },
+  offMessage : function (callback) {
+    this.off(MESSAGE_EVENT, callback);
+  },
+}
+
+function Store (obj) {
+	return _.extend({}, storeProto, EventEmitter.prototype, obj);
+}
+
+
+module.exports = Store;
