25824e49787c7b979c1577a130755da322901c4f
Brendan O'Brien <brendnaobrienesq@gmail.com>
2015-03-30 15:33:07 -0400

    stupid.

--- utils/money.js
+++ utils/money.js
@@ -3,6 +3,19 @@ var money = {
 		if (!amount) { return "$0.00"; }
 		amount = amount / 100
 
+		var DecimalSeparator = Number("1.2").toString().substr(1,1);
+		var AmountWithCommas = amount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+		var arParts = String(AmountWithCommas).split(DecimalSeparator);
+		var intPart = arParts[0];
+		var decPart = (arParts.length > 1 ? arParts[1] : '');
+		decPart = (decPart + '00').substr(0,2);
+
+		return '$' + intPart + DecimalSeparator + decPart;
+	},
+	formatLocaleCurrency : function (amount) {
+		if (!amount) { return "$0.00"; }
+		amount = amount / 100
+
 		var DecimalSeparator = Number("1.2").toLocaleString().substr(1,1);
 		var AmountWithCommas = amount.toLocaleString();
 		var arParts = String(AmountWithCommas).split(DecimalSeparator);
